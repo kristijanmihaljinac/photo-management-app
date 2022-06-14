cc1f86add639059abb89ef78a7e4b40df6441d4e
Kristijan Mihaljinac <kristijanm@in2.hr>
2022-06-14 21:19:43 +0200

    Added specifications interface and base class and IReadOnlyRepository interface

---
+++ PhotoManagementApp/Common/Domain/IReadOnlyRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Common.Domain
+{
+
+    public interface IReadOnlyRepository
+    {
+        //Example methods
+        //Task<T> GetByIdAsync(int id);
+        //Task<List<T>> GetAllAsync();
+        //Task<IEnumerable<T>> FindWithSpecificationPatternAsync(ISpecification<T> specification = null);
+    }
+}
---
+++ PhotoManagementApp/Common/Specification/ISpecification.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+
+namespace Common.Specification
+{
+    /// <summary>
+    /// This is a minimal implementation only. Let me explain each of the declared method definitions.
+    /// Criteria – This is where you can add in the expressions based on your entity.
+    /// Includes – If you want to include foreign keyed table data, you could add it using this method.
+    /// OrderBy and OrderByDescending are quite self-explanatory.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface ISpecification<T>
+    {
+        Expression<Func<T, bool>> Criteria { get; }
+        List<Expression<Func<T, object>>> Includes { get; }
+        Expression<Func<T, object>> OrderBy { get; }
+        Expression<Func<T, object>> OrderByDescending { get; }
+        public int? Skip { get;  }
+        public int? Take { get;  }
+    }
+}
