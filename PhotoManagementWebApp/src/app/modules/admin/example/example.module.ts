2393099624c646a5ecf143febaad9ce3405845c6
Kristijan Mihaljinac <kristijanm@in2.hr>
2022-07-06 15:25:05 +0200

    Added prime ng components

--- PhotoManagementWebApp/src/app/modules/admin/example/example.module.ts
+++ PhotoManagementWebApp/src/app/modules/admin/example/example.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
 import { ExampleComponent } from 'app/modules/admin/example/example.component';
+import { SharedModule } from 'app/shared/shared.module';
+
+
 
 const exampleRoutes: Route[] = [
     {
@@ -14,7 +17,8 @@ const exampleRoutes: Route[] = [
         ExampleComponent
     ],
     imports     : [
-        RouterModule.forChild(exampleRoutes)
+        SharedModule,
+        RouterModule.forChild(exampleRoutes)        
     ]
 })
 export class ExampleModule
