56b1fba8640971a5cb98b3d7b46daac540851c93
Kristijan Mihaljinac <kristijan.mihaljinac@gmail.com>
2022-08-02 19:29:22 +0200

    Design changes

--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -11,10 +11,12 @@ import { EventService } from './demo/service/event.service';
 import { IconService } from './demo/service/icon.service';
 import { NodeService } from './demo/service/node.service';
 import { PhotoService } from './demo/service/photo.service';
+import { PackageOverviewComponent } from './modules/owner/packages/package/package-overview/package-overview.component';
+import { PackageManageComponent } from './modules/owner/packages/package/package-manage/package-manage.component';
 
 @NgModule({
     declarations: [
-        AppComponent, NotfoundComponent
+        AppComponent, NotfoundComponent, PackageOverviewComponent, PackageManageComponent
     ],
     imports: [
         AppRoutingModule,
